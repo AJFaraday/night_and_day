4e83c5c0ed8b9a71e6ca0d63a2d33a08ed8a3531
andrew james faraday <ajfaraday@batman.alces.co.uk>
2019-06-07 13:13:45 +0100

    Issue #11 - Tidied up level mapping

--- js/map_data.js
+++ js/map_data.js
@@ -21,179 +21,90 @@ map_data = {
     "b": {method: "add_key", sprite: "brown_key", name: "brown_key"}
   },
   "000-home": {
-    "T": {method: "add_text", text: "Welcome to Night and Day"},
-    "d": {method: "add_text", text: "Enter the door to start"},
-    "s": {method: "add_text", text: "Keep walking for settings"},
-    "v": {method: "add_text", text: "Volume"},
-    "D": {method: "add_door", target: "001-boxes"},
-    "h": {
-      method: "add_text",
-      text: "Press the DOWN arrow to enter doors.",
-      fontSize: "16px"
-    }
+    "T": [
+      {method: "add_text", text: "Volume"},
+      {method: "add_text", text: "Welcome to Night and Day"},
+      {method: "add_text", text: "Enter the door to start"},
+      {method: "add_text", text: "Keep walking for settings"},
+      {method: "add_text", text: "Press the DOWN arrow to enter doors.", fontSize: "16px"}
+    ],
+    "D": {method: "add_door", target: "001-boxes"}
   },
   "001-boxes": {
     "D": {method: "add_door", target: "002-doors"},
-    "h": {
-      method: "add_text",
-      text: "Jump with the UP arrow.",
-      fontSize: "16px"
-    }
+    "T": {method: "add_text", text: "Jump with the UP arrow.", fontSize: "16px"}
   },
   "002-doors": {
-    "1": {method: "add_door", name: "1", target: "2"},
-    "2": {method: "add_door", name: "2", target: "1"},
-    "3": {method: "add_door", target: "003-keys"}
+    "d": [
+      {method: "add_door", name: "1", target: "2"},
+      {method: "add_door", name: "2", target: "1"}
+    ],
+    "D": {method: "add_door", target: "003-keys"}
   },
-  // TODO add_key, interaction with key, sprite name for door
   "003-keys": {
-    "R": {
-      method: "add_door",
-      name: "R",
-      target: "D",
-      sprite: "red_door",
-      required: "red_key"
-    },
-    "D": {method: "add_door", name: "D", target: "R", sprite: "red_door"},
-    "G": {
-      method: "add_door",
-      name: "G",
-      target: "004-breaking",
-      sprite: "green_door",
-      required: "green_key"
-    },
-    "h": {
-      method: "add_text",
-      text: "Some doors need keys to be opened.",
-      fontSize: "16px"
-    }
+    "R": [
+      {method: "add_door", name: "R", target: "D", sprite: "red_door", required: "red_key"},
+      {method: "add_door", name: "D", target: "R", sprite: "red_door", required: "red_key"}
+    ],
+    "G": {method: "add_door", name: "G", target: "004-breaking", sprite: "green_door", required: "green_key"},
+    "h": {method: "add_text", text: "Some doors need keys to be opened.", fontSize: "16px"}
   },
   "004-breaking": {
     "D": {method: "add_door", target: "005-maze"},
-    "j": {
-      method: "add_text",
-      text: "That floor looks weak.",
-      fontSize: "16px"
-    },
-    "h": {
-      method: "add_text",
-      text: "Try pressing DOWN while in the air.",
-      fontSize: "16px"
-    },
-    "r": {
-      method: "add_text",
-      text: "Trapped? Press R to restart.",
-      fontSize: "16px"
-    }
+    "T": [
+      {method: "add_text", text: "That floor looks weak.", fontSize: "16px"},
+      {method: "add_text", text: "Try pressing DOWN while in the air.", fontSize: "16px"},
+      {method: "add_text", text: "Trapped? Press R to restart.", fontSize: "16px"}
+    ]
   },
   "005-maze": {
     "O": {
       method: "add_box",
       offsetY: 0.5
     },
-    "R": {
-      method: "add_door",
-      name: "R",
-      sprite: "red_door",
-      required: "red_key",
-      target: "T"
-    },
-    "T": {
-      method: "add_door",
-      name: "T",
-      sprite: "red_door",
-      required: "red_key",
-      target: "R"
-    },
-
-    "G": {
-      method: "add_door",
-      name: "G",
-      target: "H",
-      sprite: "green_door",
-      required: "green_key"
-    },
-    "H": {
-      method: "add_door",
-      name: "H",
-      target: "G",
-      sprite: "green_door",
-      required: "green_key"
-    },
-
-    "D": {
-      method: "add_door",
-      name: "D",
-      target: "006-sliders",
-      required: "brown_key"
-    },
-    "h": {
-      method: "add_text",
-      text: "Press DOWN to bounce higher.",
-      fontSize: "16px"
-    }
+    "R": [
+      {method: "add_door", name: "R1", sprite: "red_door", required: "red_key", target: "R2"},
+      {method: "add_door", name: "R2", sprite: "red_door", required: "red_key", target: "R1"}
+    ],
+    "G": [
+      {method: "add_door", name: "G1", target: "G2", sprite: "green_door", required: "green_key"},
+      {method: "add_door", name: "G2", target: "G1", sprite: "green_door", required: "green_key"}
+    ],
+    "D": {method: "add_door", name: "D", target: "006-sliders", required: "brown_key"},
+    "T": {method: "add_text", text: "Press DOWN to bounce higher.", fontSize: "16px"}
   },
   "006-sliders": {
     "D": {method: "add_door", name: "D", target: "007-night"},
 
-    "R": {
-      method: "add_door",
-      name: "R",
-      sprite: "red_door",
-      required: "red_key",
-      target: "T"
-    },
-    "T": {
-      method: "add_door",
-      name: "T",
-      sprite: "red_door",
-      required: "red_key",
-      target: "R"
-    },
+    "R":
+      [
+        {method: "add_door", name: "R1", sprite: "red_door", required: "red_key", target: "R2"},
+        {method: "add_door", name: "R2", sprite: "red_door", required: "red_key", target: "R1"}
+      ],
 
-    "G": {
-      method: "add_door",
-      name: "G",
-      target: "H",
-      sprite: "green_door",
-      required: "green_key"
-    },
-    "F": {
-      method: "add_door",
-      name: "H",
-      target: "G",
-      sprite: "green_door",
-      required: "green_key"
-    },
-    "h": {
-      method: "add_text",
-      text: "Slam the slider",
-      fontSize: "16px"
-    },
-    "j": {
-      method: "add_text",
-      text: "to move it",
-      fontSize: "16px"
-    }
+    "G": [
+      {method: "add_door", name: "G1", target: "G2", sprite: "green_door", required: "green_key"},
+      {method: "add_door", name: "G2", target: "G1", sprite: "green_door", required: "green_key"}
+    ],
+    "T": [
+      {method: "add_text", text: "Slam the slider", fontSize: "16px"},
+      {method: "add_text", text: "to move it", fontSize: "16px"}
+    ]
   },
   "007-night": {
-    "D": {method: "add_door", target: "d", name: "D"},
-    "d": {method: "add_door", target: "D", name: "d"},
+    "D": [
+      {method: "add_door", target: "D2", name: "D1"},
+      {method: "add_door", target: "D1", name: "D2"}
+    ],
     "E": {method: "add_door", target: "008-many-doors"},
-    "R": {method: "add_door", target: "r", sprite: "red_door", name: "R"},
-    "r": {method: "add_door", target: "R", sprite: "red_door", name: "r"},
-    "h": {
-      method: "add_text",
-      text: "Oops, nothing over here...",
-      fontSize: "16px",
-      fill: "#fff"
-    },
-    "j": {
-      method: "add_text",
-      text: "Maybe take a leap of faith?",
-      fontSize: "16px",
-      fill: "#fff"
-    }
+    "R": [
+      {method: "add_door", target: "r", sprite: "red_door", name: "R"},
+      {method: "add_door", target: "R", sprite: "red_door", name: "r"}
+    ],
+    "T": [
+      {method: "add_text", text: "Oops, nothing over here...", fontSize: "16px", fill: "#fff"},
+      {method: "add_text", text: "Maybe take a leap of faith?", fontSize: "16px", fill: "#fff"}
+    ]
   },
   "008-many-doors": {
     "D": [
@@ -221,15 +132,11 @@ map_data = {
     "R": {method: "add_door", target: "000-home", sprite: "red_door", required: "red_key"}
   },
   "TEST-000-night-and-day": {
-    "3": {method: "add_floor"},
-    "4": {method: "add_floor"},
-    "D": {method: "add_door", target: "F", name: "D"},
-    "F": {method: "add_door", target: "D", name: "F"},
-    "G": {
-      method: "add_door",
-      target: "007-night-and-day",
-      sprite: "green_door"
-    },
+    "D": [
+      {method: "add_door", target: "F", name: "D"},
+      {method: "add_door", target: "D", name: "F"}
+    ],
+    "G": {method: "add_door", target: "007-night-and-day", sprite: "green_door"},
     "R": {method: "add_door", target: "007-night-and-day", sprite: "red_door"}
   }
 };
